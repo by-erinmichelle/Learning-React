d0bfc55c56865eb7140600742b20411f2cca45fa
Erin <erin@bluecadet.com>
2018-06-21 13:45:12 -0400

    events, refs, and this binding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -8,6 +8,25 @@ import { getFunName } from '../helpers';
 // best practice for caps
 // or written class NameHere extends Component
 class StorePicker extends React.Component {
+	// handleClick() {
+	// 	alert('hey!');
+	// }
+	// this is used with goToStore(event) syntax
+	// constructor() {
+	// 	super();
+	// 	// must bind or 'this' is undefined
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+	myInput = React.createRef();
+	// If you need to access 'this' inside of a custom method, use this syntax (and avoid having to bind all methods)
+	goToStore = event => {
+	// goToStore(event) {
+		// 1. stop form from submitting
+		event.preventDefault();
+		// 2. get the text from that input. dont touch the dom!
+		console.log(this);
+		// 3. change the page to store/whatever they need
+	}
 // every class needs at least one method: render
 	render() {
 		// need space between return and ()
@@ -15,9 +34,12 @@ class StorePicker extends React.Component {
 		return (
 			<React.Fragment>
 				{/* comment */}
-				<form className="store-selector">
+				<form className="store-selector" onSubmit={this.goToStore}>
 					<h2>please enter a store</h2>
-					<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				{/*no parentheses bc we don't want it on page load*/}
+				{/*<button onClick={this.handleClick}>Click me!</button>*/}
+					
+					<input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
 					<button type="submit">Visit Store >></button>
 				</form>
 			</React.Fragment>
